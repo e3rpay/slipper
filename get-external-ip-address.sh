e12d11163e75c6dbb69f6504a7290ade2a6ef162
Peer Xu <pppeerxu@gmail.com>
2015-04-03 22:30:19 +0800

    add get-external-ip-address.sh script

---
+++ get-external-ip-address.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+EXTERNAL_ADDRESS = $(curl http://ipecho.net/plain)
+
+if [ -e "cached" ]; then
+  CACHED_ADDRESS = $(cat cached)
+else
+  CACHED_ADDRESS = "UNCACHED"
+fi
+
+if [[ "x$EXTERNAL_ADDRESS" != "x$CACHED_ADDRESS" ]]; then
+  echo -n $EXTERNAL_ADDRESS > cached
+fi
+
+git commit
+git push origin master
